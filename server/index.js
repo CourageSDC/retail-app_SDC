5fc0933981971df641d99c263b118f1ba39ad0c9
crypto-bender <dac3mf@virginia.edu>
2022-07-25 20:46:49 -1000

    enabled text compression to increase page load performance with express compression plugin to serve dynamic assets and gzip to serve static assets

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const path = require('path');
 const cors = require('cors');
 const router = require('./routes');
 const logger = require('./middleware/logger');
-
+const compression = require('compression');
+const expressGzipStatic = require('express-static-gzip');
 const app = express();
+
+app.use(compression());
+
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(expressGzipStatic(path.join(__dirname, '../client/dist')));
 app.use(cors());
 app.use(logger);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -21,5 +22,9 @@ module.exports = {
         },
       },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 };
